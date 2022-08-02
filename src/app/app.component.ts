0a63012537d78d25507e8dc94b96babfc11343c6
John Drake <jdrake@uoregon.edu>
2022-08-02 00:17:31 -0500

    Messing around with calc.component.html to get ideas how the layout will eventually look. Also deleted default app.component.html code and replaced with h1 title, which I changed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ang_calc';
+  title = 'My first Angular App, a Calculator!';
 }
